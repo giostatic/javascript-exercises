9c31392556a5b23ec16fa43eb06d157bd40713d2
giostatic <116467840+giostatic@users.noreply.github.com>
2023-11-05 09:45:29 -1000

    changed array method to for loop

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function(arr, num) {
-    let arrFilter = arr.filter([arr],'num')
-    return arrFilter;
+const removeFromArray = function(array, removedItem){
+    let arrayWithoutRemovedItem = [];
+    for (let i = 0; i < array.length; i++){
+        if(array[i] === removedItem){
+            arrayWithoutRemovedItem.push(array[i]);
+        }
+        return arrayWithoutRemovedItem;
+    }
 };
 
 // Do not edit below this line
