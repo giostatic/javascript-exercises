5fb07de32030fa7361fd1689ef522623ace166bc
giostatic <116467840+giostatic@users.noreply.github.com>
2023-11-05 00:08:44 -1000

    utlized string split, reverse array, and joined array

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function(stringArray) {
-    stringArray.reverse('');
+const reverseString = function(string) {
+    let splitString = string.split('');
+    let reverseArray = splitString.reverse('');
+    let joinArray = reverseArray.join('');
+    return joinArray;
 };
 
 // Do not edit below this line
