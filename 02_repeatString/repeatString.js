613f8810f86780c0a33507fa979c225b983bab28
giostatic <116467840+giostatic@users.noreply.github.com>
2023-11-04 15:42:03 -1000

    changed position of array variable

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,10 @@
+const arr = [];
+
 const repeatString = function(string, times) {
     for (let i = 0; i < times; i++){
         arr.push(string);
     }
 };
-const arr = [];
 arr.join('');
 // Do not edit below this line
 module.exports = repeatString;
